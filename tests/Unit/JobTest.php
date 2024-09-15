2f249b7813e8e55d36236e85c804a6f6fc1b2e74
miraz66 <mirajulislam.com>
2024-09-16 03:19:15 +0600

    Make a Test

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Job;
+use App\Models\Tag;
 use App\Models\Employer;
 
 it('belongs to an employer', function () {
@@ -9,7 +10,19 @@ it('belongs to an employer', function () {
     $job = Job::factory()->create([
         'employer_id' => $employer->id
     ]);
+
     //  Act & Assert
-    // expect($job->employer)->toEqual($employer);
     expect($job->employer->is($employer))->toBeTrue();
+});
+
+it('has many tags', function () {
+    // Arrange
+    $job = Job::factory()->create();
+
+    // Act: Add tags to the job
+    $job->tag('Frontend');
+    $job->tag('Backend');
+
+    // Assert: Check if the job has the correct number of tags
+    expect($job->tags)->toHaveCount(2); // Access the 'tags' relationship
 });
